3375b0d45438293a5622ec7be34213868b9d3a01
shivaramr <shivaramr.mec@gmail.com>
2025-03-01 11:34:14 +0530

    Lint / Prettier Changes

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,39 +1,33 @@
-import AppointmentForm from "@/components/forms/AppointmentForm";
-import { getPatient } from "@/lib/actions/patient.actions";
 import Image from "next/image";
+import { getPatient } from "@/lib/actions/patient.actions";
+import AppointmentForm from "@/components/forms/AppointmentForm";
 
-export default async function NewAppointment({params:{userId}}:SearchParamProps) {
+export default async function NewAppointment({ params: { userId } }: SearchParamProps) {
   const patient = await getPatient(userId);
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
-      <div className="sub-container max-w-[860px] flex-1 justify-between">
-        <Image src="/assets/icons/logo-full.svg"
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <AppointmentForm type="create" userId={userId} patientId={patient.$id} />
+
+          <p className="copyright mt-10 py-12">© 2025 CarePulse</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
         height={1000}
         width={1000}
         alt="patient"
-        className="mb-12 h-10 w-fit"
-        />
-
-        <AppointmentForm 
-        type="create"
-        userId={userId}
-        patientId={patient.$id}
-        />
-
-          <p className="copyright mt-10 py-12">
-        © 2025 CarePulse
-          </p>
-         
-      </div>
-      </section>
-
-      <Image 
-      src="/assets/images/appointment-img.png"
-      height={1000}
-      width={1000}
-      alt="patient"
-      className="side-img max-w-[390px] bg-bottom"
+        className="side-img max-w-[390px] bg-bottom"
       />
     </div>
   );
