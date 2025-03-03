d10192b0e2b4b0971bac2a88f75131313588b85e
shivaramr <shivaramr.mec@gmail.com>
2025-03-03 11:35:19 +0530

    Lint / Prettier Changes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,61 +1,60 @@
-import {DataTable} from '@/components/table/DataTable'
-import StatCard from '@/components/StatCard'
-import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
-import Image from 'next/image'
-import Link from 'next/link'
-import {columns} from '@/components/table/columns'
-
+import Link from "next/link";
+import Image from "next/image";
+import StatCard from "@/components/StatCard";
+import { columns } from "@/components/table/columns";
+import { DataTable } from "@/components/table/DataTable";
+import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 
 const Admin = async () => {
-const appointments = await getRecentAppointmentList()
+  const appointments = await getRecentAppointmentList();
 
   return (
-    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
-        <header className='admin-header'>
-            <Link href='/' className='cursor-pointer'>
-            <Image 
-                src='/assets/icons/logo-full.svg'
-                height={32}
-                width={162}
-                alt='logo'
-                className='h-8 w-fit'
-            />
-            </Link>
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={32}
+            width={162}
+            alt="logo"
+            className="h-8 w-fit"
+          />
+        </Link>
 
-            <p className='text-16-semibold'> Admin Dashboard</p>
-        </header>
+        <p className="text-16-semibold"> Admin Dashboard</p>
+      </header>
 
-        <main className='admin-main'>
-            <section className='w-full space-y-4'>
-                <h1 className='header'> Welcome 👋</h1>
-                <p className='text-dark-700'>Start the day with managing new appointments</p>
-            </section>
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header"> Welcome 👋</h1>
+          <p className="text-dark-700">Start the day with managing new appointments</p>
+        </section>
 
-            <section className='admin-stat'>
-                <StatCard 
-                    type='appointments'
-                    count={appointments.scheduledCount}
-                    label='Scheduled Appointments'
-                    icon='/assets/icons/appointments.svg'
-                />
-                <StatCard 
-                    type='pending'
-                    count={appointments.pendingCount}
-                    label='Pending Appointments'
-                    icon='/assets/icons/pending.svg'
-                />
-                <StatCard 
-                    type='cancelled'
-                    count={appointments.cancelledCount}
-                    label='Cancelled Appointments'
-                    icon='/assets/icons/cancelled.svg'
-                />
-            </section>
+        <section className="admin-stat">
+          <StatCard
+            type="appointments"
+            count={appointments.scheduledCount}
+            label="Scheduled Appointments"
+            icon="/assets/icons/appointments.svg"
+          />
+          <StatCard
+            type="pending"
+            count={appointments.pendingCount}
+            label="Pending Appointments"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard
+            type="cancelled"
+            count={appointments.cancelledCount}
+            label="Cancelled Appointments"
+            icon="/assets/icons/cancelled.svg"
+          />
+        </section>
 
-            <DataTable columns={columns} data={appointments.documents}/>
-        </main>
+        <DataTable columns={columns} data={appointments.documents} />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
