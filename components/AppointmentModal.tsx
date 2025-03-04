ebb1d61fddd9713bb874034af1c7533a721717fe
shivaramr <shivaramr.mec@gmail.com>
2025-03-04 11:50:56 +0530

    Lint / Prettier Changes

--- components/AppointmentModal.tsx
+++ components/AppointmentModal.tsx
@@ -1,55 +1,56 @@
-'use client'
+"use client";
 
+import { useState } from "react";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { useState } from "react"
-import { Button } from "./ui/button"
-import AppointmentForm from "./forms/AppointmentForm"
-import { Appointment } from "@/types/appwrite.types"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { Appointment } from "@/types/appwrite.types";
+import AppointmentForm from "./forms/AppointmentForm";
 
-  
-const AppointmentModal = ({type,patientId,
-    userId,
-    appointment}:{
-    type: 'schedule' | 'cancel',
-    patientId: string,
-    userId: string,
-    appointment?: Appointment,
+const AppointmentModal = ({
+  type,
+  patientId,
+  userId,
+  appointment,
+}: {
+  type: "schedule" | "cancel";
+  patientId: string;
+  userId: string;
+  appointment?: Appointment;
 }) => {
-    const [open, setOpen] = useState(false)
-    
+  const [open, setOpen] = useState(false);
+
   return (
-<Dialog open={open} onOpenChange={setOpen}>
-  <DialogTrigger asChild>
-    <Button variant='ghost' className={`capitalize ${type==='schedule'&&'text-green-500'}`}>
-{type}
-    </Button>
-  </DialogTrigger>
-  <DialogContent className="shad-dialog sm:max-w-md">
-    <DialogHeader className="mb-4 space-y-3">
-      <DialogTitle className="capitalize ">{type} Appointment</DialogTitle>
-      <DialogDescription>
-        Please fill in the following details to {type} an appointment
-      </DialogDescription>
-    </DialogHeader>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="ghost" className={`capitalize ${type === "schedule" && "text-green-500"}`}>
+          {type}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog sm:max-w-md">
+        <DialogHeader className="mb-4 space-y-3">
+          <DialogTitle className="capitalize ">{type} Appointment</DialogTitle>
+          <DialogDescription>
+            Please fill in the following details to {type} an appointment
+          </DialogDescription>
+        </DialogHeader>
 
-    <AppointmentForm 
-        userId={userId}
-        patientId={patientId}
-        type={type}
-        appointment={appointment}
-        setOpen={setOpen}
-    />
+        <AppointmentForm
+          userId={userId}
+          patientId={patientId}
+          type={type}
+          appointment={appointment}
+          setOpen={setOpen}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-  </DialogContent>
-</Dialog>
-  )
-}
-
-export default AppointmentModal
+export default AppointmentModal;
--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,52 +1,45 @@
-"use client"
+"use client";
 
-import { convertFileToUrl } from '@/lib/utils'
-import Image from 'next/image'
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import Image from "next/image";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { convertFileToUrl } from "@/lib/utils";
 
 type FileUploaderProps = {
-    files: File[]|undefined,
-    onChange:(files:File[])=>void
-}
+  files: File[] | undefined;
+  onChange: (files: File[]) => void;
+};
 
-const FileUploader =({files,onChange}:FileUploaderProps)=> {
-    const onDrop = useCallback((acceptedFiles:File[]) => {
-        onChange(acceptedFiles)
-        }, [])
-    const {getRootProps, getInputProps} = useDropzone({onDrop})
-  
-    return (
-      <div {...getRootProps()} 
-      className='file-upload'
-      >
-        <input {...getInputProps()} />
-        {files && files?.length > 0 ?(
-            <Image 
-            src={convertFileToUrl(files[0])}
-            width={1000}
-            height={1000}
-            alt='Uploaded image'
-            className='max-h-[400px] overflow-hidden object-cover'
-            />
-        ):<>
-        <Image src='/assets/icons/upload.svg'
-        height={40}
-        width={40}
-        alt='upload'
+const FileUploader = ({ files, onChange }: FileUploaderProps) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    onChange(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className="file-upload">
+      <input {...getInputProps()} />
+      {files && files?.length > 0 ? (
+        <Image
+          src={convertFileToUrl(files[0])}
+          width={1000}
+          height={1000}
+          alt="Uploaded image"
+          className="max-h-[400px] overflow-hidden object-cover"
         />
-        <div className='file-upload_label'>
-        <p className='text-14-regular'>
-            <span className='text-green-500'>
-Click to upload
-            </span> or drag and drop
-        </p>
-        <p>SVG, PNG, JPG or Gif (max 800x400)</p>
-        </div>
-        </>}
-       
-      </div>
-    )
-  }
+      ) : (
+        <>
+          <Image src="/assets/icons/upload.svg" height={40} width={40} alt="upload" />
+          <div className="file-upload_label">
+            <p className="text-14-regular">
+              <span className="text-green-500">Click to upload</span> or drag and drop
+            </p>
+            <p>SVG, PNG, JPG or Gif (max 800x400)</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default FileUploader;
