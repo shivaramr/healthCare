f24c77bbb4ead8c6139aaa99bc0ba11da81f6ef7
shivaramr <shivaramr.mec@gmail.com>
2025-03-05 10:49:50 +0530

    Lint / Prettier Changes

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Form } from "@/components/ui/form";
-import CustomFormField from "../CustomFormField";
-import SubmitButton from "../SubmitButton";
 import { useState } from "react";
-import { UserFormValidation } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import SubmitButton from "../SubmitButton";
+import { Form } from "@/components/ui/form";
 import { useRouter } from "next/navigation";
+import CustomFormField from "../CustomFormField";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserFormValidation } from "@/lib/validation";
 import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
@@ -38,11 +38,9 @@ const PatientForm = () => {
     setIsLoading(true);
     try {
       const userData = { name, email, phone };
-
       const user = await createUser(userData);
 
       if (user) {
-
         router.push(`/patients/${user.$id}/register`);
       }
     } catch (error) {
