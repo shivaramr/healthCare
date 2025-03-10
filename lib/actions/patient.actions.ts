c87c5f3dfa067769f50bb047d183c6abc632e9e8
shivaramr <shivaramr.mec@gmail.com>
2025-03-10 10:43:49 +0530

    Lint / Prettier Changes

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,10 +1,18 @@
-'use server'
+"use server";
 
 import { ID, Query } from "node-appwrite";
-import { BUCKET_ID, DATABASE_ID, databases, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID, storage, users } from "../appwrite.config";
+import {
+  users,
+  storage,
+  ENDPOINT,
+  BUCKET_ID,
+  databases,
+  DATABASE_ID,
+  PROJECT_ID,
+  PATIENT_COLLECTION_ID,
+} from "../appwrite.config";
+import { InputFile } from "node-appwrite/file";
 import { parseStringify } from "../utils";
-
-import {InputFile} from "node-appwrite/file";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -13,7 +21,6 @@ export const createUser = async (user: CreateUserParams) => {
     return parseStringify(newuser);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
-
     if (error && error?.code === 409) {
       const documents = await users.list([Query.equal("email", [user.email])]);
 
@@ -22,30 +29,32 @@ export const createUser = async (user: CreateUserParams) => {
   }
 };
 
-export const getUser = async (userId:string) =>{
-    try {
-        const user = await users.get(userId);
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
 
-        return parseStringify(user);
-    } catch (error) {
-     console.log(error);
-        
-    }
-}
+    return parseStringify(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const registerPatient = async ({identificationDocument, ...patient}:RegisterUserParams) =>{
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
   try {
     let file;
 
-    if(identificationDocument){
+    if (identificationDocument) {
       const inputFile = InputFile.fromBuffer(
-        identificationDocument?.get('blobFile') as Blob,
-        identificationDocument?.get('fileName') as string
-      )
+        identificationDocument?.get("blobFile") as Blob,
+        identificationDocument?.get("fileName") as string
+      );
 
-      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile)
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
     }
-    
+
     const newPatient = await databases.createDocument(
       DATABASE_ID!,
       PATIENT_COLLECTION_ID!,
@@ -53,30 +62,24 @@ export const registerPatient = async ({identificationDocument, ...patient}:Regis
       {
         identificationDocumentId: file?.$id || null,
         identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
-        ...patient
+        ...patient,
       }
-    )
-    
-    return parseStringify(newPatient)
+    );
+
+    return parseStringify(newPatient);
   } catch (error) {
     console.log(error);
-    
   }
-}
+};
 
-export const getPatient = async (userId:string) =>{
+export const getPatient = async (userId: string) => {
   try {
-      const patients = await databases.listDocuments(
-        DATABASE_ID!,
-        PATIENT_COLLECTION_ID!,
-        [
-          Query.equal('userId',userId)
-        ]
-      );
+    const patients = await databases.listDocuments(DATABASE_ID!, PATIENT_COLLECTION_ID!, [
+      Query.equal("userId", userId),
+    ]);
 
-      return parseStringify(patients.documents[0]);
+    return parseStringify(patients.documents[0]);
   } catch (error) {
-   console.log(error);
-      
+    console.log(error);
   }
-}
+};
